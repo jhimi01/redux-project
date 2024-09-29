6d74572029eca37482aef5f211a7585a2ef1e1e3
jhimi01 <talukderjhimi@gmail.com>
2024-09-29 20:32:32 +0600

    redux connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Home from "./pages/Home"
+
 function App() {
 
   return (
     <>
-      <h1 className="capitalize text-2xl">redux</h1>
+      <Home />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      this is a Home
+    </div>
+  )
+}
+
+export default Home
