29b265c6c82ce5cc938d8571b2d3a0fb1a6fdab4
jhimi01 <talukderjhimi@gmail.com>
2024-10-02 11:44:19 +0600

    increament decreament

---
+++ src/redux/features/cart/cartSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: { },
+});
+
+
+export default counterSlice.reducer;
